cea1a8414c802aee0aa6cff4ff621d218af77deb
Boris Hernández <borisnandez93@gmail.com>
2021-09-19 15:25:07 -0600

    cambios

--- Tarea_1-(Boris Hernandez)/Program.cs
+++ Tarea_1-(Boris Hernandez)/Program.cs
@@ -6,7 +6,39 @@ namespace Tarea_1__Boris_Hernandez_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //El usuario tendra 3 intentos
+            //usuario: user00 contra: contra123
+            contraseña();
+            
+
+            static void contraseña()
+            {
+                int intentos = 0, verif = 0;
+                string pass, user;
+                do
+                {
+                    Console.Write("DIGITE USUARIO: "); user = Console.ReadLine();
+                    Console.Write("DIGITE CLAVE: "); pass = Console.ReadLine();
+                    if ((pass == "contra123") && (user == "user00"))
+                    {
+                        verif = 1;
+                    }
+                    else
+                    {
+                        intentos++;
+                    }
+                    //aumentara el numero de intentos si no es correcta la contraseña
+                } while (((intentos < 3) & (verif == 0)));
+                if (verif == 1)
+                {
+                    Console.WriteLine("BIENVENIDO ");
+                }
+                else
+                {
+                    Console.WriteLine("OPORTUNIDADES TERMINADAS");
+                }
+            }
+
         }
     }
 }
