5fda5410a8091f5b39d96daa2b4f8db4c290aeac
Boris Hernández <borisnandez93@gmail.com>
2021-09-19 16:04:11 -0600

    cambios

--- Tarea_1-(Boris Hernandez)/Program.cs
+++ Tarea_1-(Boris Hernandez)/Program.cs
@@ -9,7 +9,49 @@ namespace Tarea_1__Boris_Hernandez_
             //El usuario tendra 3 intentos
             //usuario: user00 contra: contra123
             contraseña();
-            
+
+
+            int opcion = 0;
+            string valor = "";
+            // Mostramos el menu
+            Console.WriteLine("1-Deposito");
+            Console.WriteLine("2-Retiro");
+            Console.WriteLine("3-Consulta de saldo");
+            Console.WriteLine("4-Cambio de contraseña");
+            // Pedimos la opcion
+            Console.WriteLine("Cual es tu opcion:");
+            valor = Console.ReadLine();
+            opcion = Convert.ToInt32(valor);
+            // Checamos por el deposito
+            if (opcion == 1)
+            {
+                Deposito();
+            }
+            // Checamos por el retiro
+            if (opcion == 2)
+            {
+                // Variable para nuestro resultado
+                float resultado = 0;
+                // Invocamos y obtenemos el resultado
+                resultado = Retiro();
+                // Mostramos el resultado
+                Console.WriteLine("El resultado de la resta es {0}", resultado);
+            }
+            // Checamos por la consulta de saldo
+            if (opcion == 3)
+            {
+                //variable para nuestro saldo
+                float saldo = 0;
+                //Invocamos y obtenemos el resultado
+                saldo = Consulta();
+                // Mostramos el saldo
+                Console.WriteLine("Su saldo es de: " + saldo);
+            }
+            // Checamos por el cambio de contraseña
+            if (opcion == 4)
+            {
+                Cambio();
+            }
 
             static void contraseña()
             {
@@ -39,6 +81,50 @@ namespace Tarea_1__Boris_Hernandez_
                 }
             }
 
+            static void Deposito()
+            {
+                // Variables necesarias
+                float a = 0;
+                float b = 0;
+                float r = 0;
+                // Pedimos los valores
+                b = 100;
+                Console.WriteLine("Ingrese la cantidad de dinero que depositara: ");
+                a = float.Parse(Console.ReadLine());
+                // Calculamos el resultado
+                r = a + b;
+                // Mostramos el resultado
+                Console.WriteLine("Su saldo es ahora de "+ r);
+            }
+
+            static float Retiro()
+            {
+                // Variables necesarias
+                float a = 0;
+                float b = 0;
+                float r = 0;
+                // Pedimos los valores
+                b = 100;
+                Console.WriteLine("Ingrese la cantidad de dinero que Retirara: ");
+                a = float.Parse(Console.ReadLine());
+                // Calculamos el resultado
+                r = a - b;
+                // Retornamos el resultado
+                return r;
+            }
+
+            static float Consulta()
+            {
+                float a = 300;
+                float s = 0;
+                s = a + 0;
+                return s;
+            }
+
+            static void Cambio()
+            {
+
+            }
         }
     }
 }
